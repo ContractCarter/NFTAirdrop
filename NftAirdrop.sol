b764c783cffb725dfac36d554bdd0c155ac4d950
ContractCarter <88419641+ContractCarter@users.noreply.github.com>
2021-09-08 10:11:23 -0400

    Create NftAirdrop.sol

---
+++ NftAirdrop.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.7;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract NFTAirdrop is ERC721 {
+    struct Airdrop {
+    address nft;
+    uint id;
+    }
+    uint public nextAirdropId;
+    address public admin;
+    mapping(uint => Airdrop) public airdrops;
+    mapping(address => bool) public recipients;
+    
+    constructor() {
+        admin = msg.sender;
+    }
+    
+    function addAirdrops(Airdrop[] memory _airdrops) external {
+        require(msg.sender == admin, 'only admin');
+        uint _nextAirdropId = nextAirdropId;
+        for(uint i = 0; i < _airdrops.length; i++) {
+        airdrops[_nextAirdropId] = _airdrops[i];
+        IERC721(_airdrops[i].nft).transferFrom(
+            msg.sender, 
+            address(this),
+            _airdrops[i].id
+            );
+            _nextAirdropId++;
+        }
+    }
+    
+    function addRecipients(address[] memory _recipients) external {
+    require(msg.sender == admin, 'only admin');
+    for(uint i = 0; i < _recipients.length; i++) {
+    recipients[_recipients[i]] = false;
+      }
+    }
+    
+    function removeRecipients(address[] memory _recipients) external {
+    require(msg.sender == admin, 'only admin');
+    for(uint i = 0; i < _recipients.length; i++) {
+    recipients[_recipients[i]] = false;
+    }
+    }
+    
+    function claim() external {
+    require(recipients[msg.sender] == true, 'recipient not registered');
+    recipients[msg.sender] = false;
+    Airdrop storage airdrop = airdrops[nextAirdropId];
+    IERC721(airdrop.nft).transferFrom(address(this), msg.sender, airdrop.id);
+    nextAirdropId++;
+    }
+}
